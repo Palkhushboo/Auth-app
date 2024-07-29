2531485f97f5c8a9be6ce4f7b5e9e1c321c69a5b
khushboo <palkhushboo2632@gmail.com>
2024-07-29 12:32:19 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+//import dotenv from 'dotenv';
+//dotenv.config();
+mongoose.connect('mongodb://localhost:27017/Auth').then(()=>{
+ console.log("connected to database")
+}).catch((err)=>{
+  console.log(err)
+})
 const app=express();
-app.listen(3000,()=>{
- console.log('listening on port 3000')
+app.listen(400,()=>{
+ console.log('listening on port 400')
 })
